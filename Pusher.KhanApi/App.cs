be6bab32bfbaa6c22c97a0efd79909f06f6b8614
Ram Hemasri <ramhemasri@gmail.com>
2013-03-16 17:54:27 +0000

    Initial commit

---
+++ Pusher.KhanApi/App.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Pusher.KhanApi
+{
+    public class App
+    {
+        private static MainViewModel viewModel = null;
+
+        /// <summary>
+        /// A static ViewModel used by the views to bind against.
+        /// </summary>
+        /// <returns>The MainViewModel object.</returns>
+        public static MainViewModel ViewModel
+        {
+            get
+            {
+                // Delay creation of the view model until necessary
+                if (viewModel == null)
+                    viewModel = new MainViewModel();
+
+                return viewModel;
+            }
+        }
+    }
+}
